ff21673915b1da7e21866b86bcbb0b084fd560c4
Carlo Maria Zwölf <carlo-maria.zwolf@obspm.fr>
2024-10-24 10:50:26 +0200

    Adding detailed comment to VAMDCQuery class

--- spectral/vamdcQuery.py
+++ spectral/vamdcQuery.py
@@ -7,80 +7,183 @@ import uuid
 
 
 def display_message(messageToDisplay, wannaDisplay):
+  """
+  This function is used to display log message in the console. 
+
+  Arguments
+  ---------
+
+  messageToDisplay : str
+    the log message to display
+
+  wannaDisplay : boolean
+    If true, the message is displayed. 
+
+  """
   if wannaDisplay:
      print(messageToDisplay)
 
 class VamdcQuery:
+    """This class is a wrapper used to submit spectroscopic queries to the VAMDC infrastructure. 
+    This class implements low level mechanisms: unless you are a developer whishing to modify this low-level behaviour, it is suggested to use
+    the high level functions and classes which interact with the VAMDCQuery class while hiding the complexity. 
+    The VAMDCQuery class wraps spectroscopic queries having the following pattern:
+    'select * where (RadTransWavelength >= lambdMin AND RadTransWavelength <= lambdaMax) AND ((InchiKey = 'InchiKey'))format(lambdaMin, lambdaMax, InchiKey)'
+    and submit this type of queries to a specif VAMDC Node.
+    
+    Attributes
+    ----------
+    nodeEndpoint : str
+      the string containing the Node TAP endpoint to submit query to
+    
+    lambdaMin : float
+      the inf boundary (in Angstrom) of the wavelenght interval
+
+    
+    lambdaMax : float
+      the sup boundary (in Angstrom) of the wavelenght interval  
+
+
+    InchiKey : str
+      the InchiKey identifier of the chemical species we want to retrieve data on
+
+    speciesType : str
+      this attribute may take two values: 'molecule' or 'atom'. We need this flag because the processing 
+      to convert the VAMDC output to a Pandas dataframe depends on the species type. 
+
+    
+    hasData : Boolean 
+      this flag is true if, while executed, the query will generate data
+
+    truncated : Boolean
+      this flag is true if, while executed, the query will be truncated (the resumt of the given query won't contain all the data available on a given data node)    
+    
+    XSAMSFileName : str
+      the name of the XSAMS output file generated while executing the query
+     
+    localUUID : str
+      A local (client side) unique identifer for the query
+    
+    verbose : boolean
+      If this flag is true, detaile log information are displayed
+    
+    Methods
+    -------
+    getXSAMSData()
+      Run the GET method on the query to dowloand the data. Query related data are stored using the XSAMSFileName name.
+    
+    
+    convertToDataFrame()
+      Convert the result of the query into a Pandas dataframe. This conversion is performed using the VAMDC-molecular
+      or VAMDC-atomic conversion processors (cf. https://github.com/VAMDC/Processors), according to the species involved in the query. 
+
+    """
 
     def __init__(self, nodeEndpoint, lambdaMin, lambdaMax, InchiKey, speciesType, totalListOfQueries, verbose = False):
-        self.nodeEndpoint = nodeEndpoint
-        self.lambdaMin = lambdaMin
-        self.lambdaMax =lambdaMax
-        self.InchiKey = InchiKey
-        self.speciesType = speciesType
-        self.hasData = False
-        self.truncated = None
-        self.XSAMSFileName = None
-        self.localUUID = None
-        self.verbose = verbose
+      """ This is the constructor of the VAMDCQuery class. 
+      The subtlety consists in the fact that this constructor is recursive and takes as argument a list of VAMDCQuery instances already instanciated. 
+      This design copes with a particularity of the VAMDC infrastructure: if the result of a given query generated too much data, the result may be truncated. 
+      HEAD requests associated with the Query give the information about this truncation. In this constructor we instanciate an initial query and we verify, by executing 
+      the corresponding HEAD request, that this is not truncated and that this contain data. In that case, we add the current instance to the list of 
+      the existing instances (passed as an call argument to the constructor). If it is truncated, we split the query into two sub-queries. 
+      This mechanism being recursive, at the end the list of instances will contain only queries which will not be truncated while executed. 
 
-        self.localUUID = str(uuid.uuid4())
+      Arguments
+      ----------
+      nodeEndpoint : str
+        the string containing the Node TAP endpoint to submit query to
+    
+      lambdaMin : float
+        the inf boundary (in Angstrom) of the wavelenght interval
 
-        message = f"\nCreating {self.localUUID} ; l_min={lambdaMin} ; l_max={lambdaMax} ;  node ={nodeEndpoint} ; inchi={InchiKey}"
-        display_message(message,verbose)
+      lambdaMax : float
+        the sup boundary (in Angstrom) of the wavelenght interval  
+  
+      InchiKey : str
+        the InchiKey identifier of the chemical species we want to retrieve data on
 
-        query = "select * where (RadTransWavelength >= {0} AND RadTransWavelength <= {1}) AND ((InchiKey = '{2}'))".format(lambdaMin, lambdaMax, InchiKey)
-        self.vamdcCall = self.nodeEndpoint + "sync?LANG=VSS2&REQUEST=doQuery&FORMAT=XSAMS&QUERY="+query
+      speciesType : str
+        this attribute may take two values: 'molecule' or 'atom'. We need this flag because the processing 
+        to convert the VAMDC output to a Pandas dataframe depends on the species type. 
 
-        # to be changed in the final version of the lib. This option desactivate the Query Store notifications
-        headers = {'User-Agent':'VAMDC Query store'}
+      verbose : boolean
+      If this flag is true, detaile log information are displayed  
+      """
+
+      self.nodeEndpoint = nodeEndpoint
+      self.lambdaMin = lambdaMin
+      self.lambdaMax =lambdaMax
+      self.InchiKey = InchiKey
+      self.speciesType = speciesType
+      self.hasData = False
+      self.truncated = None
+      self.XSAMSFileName = None
+      self.localUUID = None
+      self.verbose = verbose
+
+      self.localUUID = str(uuid.uuid4())
+
+      message = f"\nCreating {self.localUUID} ; l_min={lambdaMin} ; l_max={lambdaMax} ;  node ={nodeEndpoint} ; inchi={InchiKey}"
+      display_message(message,verbose)
+
+      query = "select * where (RadTransWavelength >= {0} AND RadTransWavelength <= {1}) AND ((InchiKey = '{2}'))".format(lambdaMin, lambdaMax, InchiKey)
+      self.vamdcCall = self.nodeEndpoint + "sync?LANG=VSS2&REQUEST=doQuery&FORMAT=XSAMS&QUERY="+query
+
+      # to be changed in the final version of the lib. This option desactivate the Query Store notifications
+      headers = {'User-Agent':'VAMDC Query store'}
          
-        try:
-            response = requests.head(self.vamdcCall, headers=headers)
+      try:
+          response = requests.head(self.vamdcCall, headers=headers)
             
-            if response.status_code == 200:
-                self.hasData = True
+          if response.status_code == 200:
+              self.hasData = True
                
-                queryTruncation = response.headers.get("VAMDC-TRUNCATED")
-                if queryTruncation is None or queryTruncation == '100' or  queryTruncation == "None":
-                    self.truncated = False
-                    message = f"__status {self.localUUID} is not truncated"
+              queryTruncation = response.headers.get("VAMDC-TRUNCATED")
+              if queryTruncation is None or queryTruncation == '100' or  queryTruncation == "None":
+                  self.truncated = False
+                  message = f"__status {self.localUUID} is not truncated"
                     
-                    display_message(message,verbose)
-                else:
-                   self.truncated = True
-                   message = f"__status {self.localUUID} is truncated"
-                   display_message(message,verbose)
-            else:
-               message = f"__status {self.localUUID} has no data"
-               display_message(message,verbose)
-            
-            # if the query has data
-            if self.hasData is True:
-              # if the query is not truncated
-              if self.truncated is False :
-                # we add to the total list
-                totalListOfQueries.append(self)
-                message = f"++++++++ {self.localUUID} added to the list of queries to execute"
-                display_message(message,verbose)
-
+                  display_message(message,verbose)
               else:
-                #if the query is truncated we split it in two
-                newFirstLambdaMin = self.lambdaMin
-                newFirstLambdaMax = 0.5*(self.lambdaMax + self.lambdaMin)
-                newSecondLambdaMin = newFirstLambdaMax
-                newSecondLambdaMax = self.lambdaMax
-                message = f"-------> {self.localUUID} splitting ; l1_min ={newFirstLambdaMin}; l1_max={newFirstLambdaMax}; l2_min={newSecondLambdaMin}; l2_max={newSecondLambdaMax}"
-                display_message(message,verbose)
-                VamdcQuery(self.nodeEndpoint, newFirstLambdaMin, newFirstLambdaMax, self.InchiKey, self.speciesType, totalListOfQueries, verbose=self.verbose)
-                VamdcQuery(self.nodeEndpoint, newSecondLambdaMin, newSecondLambdaMax, self.InchiKey, self.speciesType, totalListOfQueries, verbose=self.verbose)
+                  self.truncated = True
+                  message = f"__status {self.localUUID} is truncated"
+                  display_message(message,verbose)
+          else:
+              message = f"__status {self.localUUID} has no data"
+              display_message(message,verbose)
+            
+          # if the query has data
+          if self.hasData is True:
+            # if the query is not truncated
+            if self.truncated is False :
+              # we add to the total list
+              totalListOfQueries.append(self)
+              message = f"++++++++ {self.localUUID} added to the list of queries to execute"
+              display_message(message,verbose)
+
+            else:
+              #if the query is truncated we split it in two
+              newFirstLambdaMin = self.lambdaMin
+              newFirstLambdaMax = 0.5*(self.lambdaMax + self.lambdaMin)
+              newSecondLambdaMin = newFirstLambdaMax
+              newSecondLambdaMax = self.lambdaMax
+              message = f"-------> {self.localUUID} splitting ; l1_min ={newFirstLambdaMin}; l1_max={newFirstLambdaMax}; l2_min={newSecondLambdaMin}; l2_max={newSecondLambdaMax}"
+              display_message(message,verbose)
+              VamdcQuery(self.nodeEndpoint, newFirstLambdaMin, newFirstLambdaMax, self.InchiKey, self.speciesType, totalListOfQueries, verbose=self.verbose)
+              VamdcQuery(self.nodeEndpoint, newSecondLambdaMin, newSecondLambdaMax, self.InchiKey, self.speciesType, totalListOfQueries, verbose=self.verbose)
                 
 
-        except TimeoutError as e:
-            print("TimeOut error")
+      except TimeoutError as e:
+        print("TimeOut error")
   
 
     def getXSAMSData(self):
+      """
+      This method executes a GET request on the current query instance to extract data from the VAMDC infrastructure
+      The data extraction is performed only if the query will contain data and will not be truncated 
+      (those states are checked running HEAD request in the class constructor).
+      The dowloaded data are stored with the filename from the attribute XSAMSFileName
+      """
       # to be changed in the final version of the lib. This option desactivate the Query Store notifications
       headers = {'User-Agent':'VAMDC Query store'}
       self.queryToken = None
@@ -110,6 +213,12 @@ class VamdcQuery:
        
 
     def convertToDataFrame(self):
+       """
+       This method convert the result dowloaded (while executing the getXSAMSData on the current instance) from
+       XSAMS data-format to Pandas dataframe. 
+       THis conversion is performed by locally applying the VAMDC processors (https://github.com/VAMDC/Processors): 
+       the atomic processor if the species in the query is an atom, the molecular processor if the species in the query is a molecule
+       """
        self.lines_df = None
 
        # if the data are there (we chek the presence with the Query Token)
@@ -157,7 +266,7 @@ class VamdcQuery:
           
           self.lines_df = tableHTML[1]
 
-          # adding to the data-frame the information about the queryToken
+          # adding to the data-frame the information about the queryToken. If not available, we use the local query UUID.
           self.lines_df["queryToken"]= self.queryToken if self.queryToken is not None else (self.localUUID+self.nodeEndpoint)
 
 
