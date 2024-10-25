f7b4ccd5cb7a31be72b73f99157dd59548791e40
Carlo Maria Zwölf <carlo-maria.zwolf@obspm.fr>
2024-10-25 11:08:06 +0200

    Adding comments to the energyConverter module

--- spectral/energyConverter.py
+++ spectral/energyConverter.py
@@ -1,4 +1,7 @@
 def get_phisical_constants():
+    """
+    Defines values for fundamental physical constants
+    """
     physical_constants = {
         'c': 299792458 ,  # speed of light
         'h': 6.62607015e-34 , # Planck constant
@@ -9,6 +12,12 @@ def get_phisical_constants():
 
 
 def get_conversion_factors():
+    """
+    Defines conversion factors between units.
+    Base unit for energt is Joule.
+    Base unit for frequency is Hertz.
+    Base unit for wavelenght is meter.
+    """
     physical_constants = get_phisical_constants()
     conversion_factors = {
         'energy': {
@@ -42,6 +51,28 @@ def get_conversion_factors():
     return conversion_factors
 
 def electromagnetic_conversion(value, from_unit, to_unit):
+    """
+    Perform unit conversion between all the units generally used to represent energy for electromagnetic phenomena. Those include 
+    energy, frequencies and wavelenghts. 
+
+    Args:
+        value : float
+            The value (expressed in the unit defined by the variable 'from_unit') to convert.
+        
+        from_unit :str
+            The unit one want to convert value from. Must take one of the following values: 
+            joule, millijoule, nanojoule, picojoule, eV, erg, kelvin, rydberg, cm-1, hertz, kilohertz, megahertz
+            gigahertz, terahertz, meter, centimeter,millimeter, micrometer, nanometer, angstrom
+
+        to_unit :str
+            The unit one want to convert value to. Must take one of the following values: 
+            joule, millijoule, nanojoule, picojoule, eV, erg, kelvin, rydberg, cm-1, hertz, kilohertz, megahertz
+            gigahertz, terahertz, meter, centimeter,millimeter, micrometer, nanometer, angstrom
+        
+    Returns:
+        converted_value : float
+            The valeu converted to the unit expressed by the content of the 'to_unit' variable.  
+    """
     conversion_factors = get_conversion_factors()
     physical_constants = get_phisical_constants()
     
@@ -117,6 +148,11 @@ def electromagnetic_conversion(value, from_unit, to_unit):
    
 
 class WrappingClass:
+    """
+    This class is for internal use and not to be used by end-users. 
+    This is a wrapping, kind of hack, to call the function electromagnetic_conversion as it was a lambda function containing a single parameter. 
+    This wrapping is necessary to easily perform unit conversion in dataframe columns and is used by the convert_dataframe_units function (this last is the end-user function users should use)
+    """
     def __init__(self, from_unit, to_unit , conversion_function):
         self.from_unit = from_unit
         self.to_unit = to_unit
@@ -126,7 +162,38 @@ class WrappingClass:
         return self.conversion_function(value_to_convert, self.from_unit, self.to_unit)
 
 
-def convert_dataframe_units(input_df, input_col_name, input_col_unit, output_col_name, output_col_unit, delete_input_col):
+def convert_dataframe_units(input_df, input_col_name, input_col_unit, output_col_name, output_col_unit, delete_input_col=False):
+    """
+    Perform unit conversion for values in a specific column of a Pandas Dataframe. The conversion is between all the units generally used to represent energy for electromagnetic phenomena. Those include 
+    energy, frequencies and wavelenghts. 
+
+    Args:
+        input_df: dataframe
+            The dataframe containing the column one want to convert unit on.
+
+        input_col_name: str
+            The name of the dataframe's column one want to convert unit on.
+        
+        input_col_unit:str
+            The unit one want to convert value from. Must take one of the following values: 
+            joule, millijoule, nanojoule, picojoule, eV, erg, kelvin, rydberg, cm-1, hertz, kilohertz, megahertz
+            gigahertz, terahertz, meter, centimeter,millimeter, micrometer, nanometer, angstrom
+
+        output_col_name: str
+            The name of the dataframe's column where converted values will be stored.
+
+        output_col_unit: str
+            The unit one want to convert value to. Must take one of the following values: 
+            joule, millijoule, nanojoule, picojoule, eV, erg, kelvin, rydberg, cm-1, hertz, kilohertz, megahertz
+            gigahertz, terahertz, meter, centimeter,millimeter, micrometer, nanometer, angstrom        
+
+        delete_input_col: boolean
+            If this flaf is true, the column containing input value is deleted. Default False.
+        
+    Returns:
+        input_df : dataframe
+            The dataframe containing a new column with the converted values.
+    """
     wrappedConversion = WrappingClass(input_col_unit, output_col_unit, electromagnetic_conversion)
     input_df[output_col_name] = input_df[input_col_name].apply(lambda x : wrappedConversion.wrapped_function(x))
 
