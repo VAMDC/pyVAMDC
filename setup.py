de69cb0bbf427871d4859fe08bf1d2e9cd08765f
Carlo Maria Zwölf <carlo-maria.zwolf@obspm.fr>
2026-06-12 08:55:37 +0200

    updating setup to reflect changement in project.toml file

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
+# All package configuration is defined in pyproject.toml.
+# This file exists only for compatibility with legacy tools.
 from setuptools import setup
 
-setup(
-    name="pyVAMDC",
-    version="0.1",
-    description="A brand-new library to interact with the VAMDC infrastructure",
-    author="Carlo Maria Zwölf, Nicolas Moreau",
-    packages=["pyVAMDC", "pyVAMDC.spectral", "pyVAMDC.radex"],
-    package_dir={
-        "pyVAMDC": ".",
-        "pyVAMDC.spectral": "./spectral",
-        "pyVAMDC.radex": "./radex",
-    },
-    package_data={"": ["./xsl/*.xsl"]},
-    include_package_data=True,
-    install_requires=[
-        "pandas",
-        "lxml",
-        "numpy>=2.0.0",
-        "requests",
-        "rdkit",
-        "duckdb>=0.9.0",
-        "pyarrow>=10.0.0",
-    ],
-)
+setup()
